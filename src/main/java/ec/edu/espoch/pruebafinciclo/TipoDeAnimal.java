07b0635accfa9a4e2656b2c2876dda596baff674
CristhiAn-555 <cristhianvalencia999@gmail.com>
2025-05-22 18:41:12 -0500

    actualizando codigo

--- src/main/java/ec/edu/espoch/pruebafinciclo/TipoDeAnimal.java
+++ src/main/java/ec/edu/espoch/pruebafinciclo/TipoDeAnimal.java
@@ -1,20 +1,17 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package ec.edu.espoch.pruebafinciclo;
 
 /**
  *
  * @author PERSONAL
  */
-public class PruebaFinCiclo {
-
-    public static void main(String[] args) {
-        
-       public String nombre;
-       public int edad;
-       
-       
-    }
+public enum TipoDeAnimal {
+    
+    PERRO,
+    GATO,
+    AVE
+    
 }
